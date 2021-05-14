5ddbc8fc241116ed8ed4b0e5587f88618dea802c
uros_delic <uros.delic@quantox.com>
2021-05-14 11:34:16 +0200

    factorial

--- practice.js
+++ practice.js
@@ -1,4 +1,8 @@
-function test() {
-  console.log("git");
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
-test();
+console.log(factorial(5));
