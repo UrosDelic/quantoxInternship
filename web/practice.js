0026e1ea4f2db15aac6744d4f0eb5343c8e414a2
Uros47 <urosdelic89@gmail.com>
2021-05-15 20:48:45 +0200

    x

--- web/practice.js
+++ web/practice.js
@@ -1,8 +1,9 @@
-
-function submit() {
-  label1.innerHTML = input1.value;
-}
-
 const button1 = document.getElementById('button');
 const label1 = document.getElementById('label');
 const input1 = document.getElementById('input');
+
+function submit() {
+  label1.innerHTML = input1.value;
+
+}
+
