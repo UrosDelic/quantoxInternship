eff4eee556b933e449e81d8eaff739182d509853
uros_delic <uros.delic@quantox.com>
2021-05-17 16:56:59 +0200

    kod premesten u objects

--- objects.js
+++ objects.js
@@ -1,15 +1,48 @@
-var myDog = {
-  name: "Coder",
-  legs: 4,
-  tails: 1,
-  friends: ["freeCodeCamp Campers"],
+// var myDog = {
+//   name: "Coder",
+//   legs: 4,
+//   tails: 1,
+//   friends: ["freeCodeCamp Campers"],
+// };
+
+// myDog.primer = "test primera";
+
+// function changeObjName(obj, name) {
+//   name = this.myDog.name;
+// }
+
+// let test = changeObjName(myDog, "testName");
+// console.log(test, "cons test");
+var recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
 };
-
-myDog.primer = "test primera";
-
-function changeObjName(obj, name) {
-  name = this.myDog.name;
+function updateRecords(records, id, prop, value) {
+  if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+    records[id][prop] = [value];
+  } else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value);
+  } else if (value === "") {
+    delete records[id][prop];
+  }
+  return records;
 }
 
-let test = changeObjName(myDog, "testName");
-console.log(test, "cons test");
+updateRecords(recordCollection, 5439, "artist", "ABBA");
