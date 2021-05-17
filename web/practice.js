4b913e1ea905aceb0bd56b60715f506be203ad7f
uros_delic <uros.delic@quantox.com>
2021-05-17 11:01:22 +0200

    reorder

--- web/practice.js
+++
@@ -1,9 +0,0 @@
-const button1 = document.getElementById('button');
-const label1 = document.getElementById('label');
-const input1 = document.getElementById('input');
-
-function submit() {
-  label1.innerHTML = input1.value;
-
-}
-
